5121534677664fd8b0a4b52b12bf50863a067164
Jyothi-Gaddameedi <g.jyothi.779@gmail.com>
2023-11-09 21:52:27 +0530

    added details about charminar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Charminar</h1>
+      <img src="./images/Charminar.webp"></img>
+      <p>Charminar, (Urdu: “Four Minarets”) historic monument located at the heart of Hyderabad, west-central Telangana state, south-central India. The city, which is the capital of both Telangana and Andhra Pradesh states, was also the capital of the historic princely state of Hyderabad.</p>
+    </div>
+  );
+}
+
+export default App;
